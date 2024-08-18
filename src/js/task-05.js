96ce3a9de9e6f08fcea2e783a56d89185ab98ccf
roxana-toma <toma.iroxana@gmail.com>
2024-08-18 22:39:45 +0200

    task 5 completed

---
+++ src/js/task-05.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const nameInput = document.getElementById('name-input');
+const nameOutput = document.getElementById('name-output');
+
+nameInput.addEventListener('input', event => {
+  const inputValue = event.target.value.trim();
+  nameOutput.textContent = inputValue === '' ? 'Anonymous' : inputValue;
+});
