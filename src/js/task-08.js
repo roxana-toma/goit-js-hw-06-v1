ab73fd89884c48032dd3aa349075d27e40cc8424
roxana-toma <toma.iroxana@gmail.com>
2024-08-18 22:46:42 +0200

    task 8 completed

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,23 @@
+'use strict';
 
+const loginForm = document.querySelector('.login-form');
+
+loginForm.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    return alert('All fields must be filled!');
+  }
+
+  const formData = {
+    email: email.value,
+    password: password.value,
+  };
+
+  console.log(formData);
+  event.currentTarget.reset();
+});
