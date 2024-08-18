50a71269e603c9d7dd586445d106d646317a1416
roxana-toma <toma.iroxana@gmail.com>
2024-08-18 22:55:36 +0200

    task 10 completed

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,5 +1,38 @@
+'use strict';
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const controls = document.querySelector('#controls');
+const boxesContainer = document.querySelector('#boxes');
+const createBtn = controls.querySelector('[data-create]');
+const destroyBtn = controls.querySelector('[data-destroy]');
+
+function createBoxes(amount) {
+  const boxes = [];
+  let size = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.push(box);
+    size += 10;
+  }
+
+  boxesContainer.append(...boxes);
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+}
+
+createBtn.addEventListener('click', () => {
+  const amount = parseInt(controls.querySelector('input').value);
+  createBoxes(amount);
+});
+
+destroyBtn.addEventListener('click', destroyBoxes);
